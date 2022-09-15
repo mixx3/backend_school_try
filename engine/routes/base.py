940c4f1c73d817df18bfd8f10cd3f58a0fd3ec5a
Mikhail <mmikee00800@gmail.com>
2022-09-15 21:03:42 +0300

    some more logs just for fun

--- engine/routes/base.py
+++ engine/routes/base.py
@@ -26,10 +26,20 @@ app.include_router(node_router)
 
 @app.exception_handler(NodeNotFound)
 async def not_found_error(request: starlette.requests.Request, exc: NodeNotFound):
-    return JSONResponse({"error": exc.args[0], "request": request.path_params}, status_code=404)
+    logger.info(f"Invalid data {exc.args[0]}, {request.path_params}")
+    return JSONResponse(content={"description": "Невалидная схема документа или входные данные не верны."},
+                        status_code=404)
 
 
 @app.exception_handler(ValueError)
 async def http_value_error_handler(request: starlette.requests.Request, exc: ValueError):
     logger.info(f"Failed to parse data, request: {request.path_params}, exc: {exc}")
-    return JSONResponse({"error": "Error"}, status_code=400)
+    return JSONResponse(content={"description":	"Элемент не найден"},
+                        status_code=400)
+
+
+@app.exception_handler(Exception)
+async def http_error_handler(req, exc):
+    logger.info(f"500 internal server error {req} \n {exc}")
+    return PlainTextResponse("Error",
+                             status_code=500)
---
+++ tests/test_valid.py
@@ -0,0 +1,3 @@
+from fastapi.testclient import TestClient
+
+from engine.routes.base import app
